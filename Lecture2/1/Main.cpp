34a521a35f2a723b0d76a4ca46de4ca091868cc5
H0RIS <H0RIS@outlook.com>
2024-09-24 13:33:21 +0200

    1

--- Lecture2/1/Main.cpp
+++ Lecture2/1/Main.cpp
@@ -4,5 +4,12 @@ int main()
 {
     std::cout << "Hello, World!" << std::endl;
 
+    int age = 33;
+    std::string first = "Ferenc";
+    std::string last = "Balogh";
+    std::string name = first + " " + last;
+
+    std::cout << name << " " << age << std::endl;
+
     return 0;
 }
